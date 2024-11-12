dac83b0eac368d828593ad5d6c5d4b19a098d0d0
Sergey-air-749 <michinssergeevich@gmail.com>
2024-11-12 20:09:16 +0500

    new commit

--- js/main playr.js
+++ js/main playr.js
@@ -12,7 +12,7 @@ if (localStorage.getItem('playr') == null) {
     audio_name_S.innerHTML = localStorage.getItem('audio_name')
     audio_after_S.innerHTML = localStorage.getItem('audio_after')
 
-    audio.duration = localStorage.getItem('audio_time')
+    audio_after_S.duration = localStorage.getItem('audio_time')
 
     InputReg_S.max = localStorage.getItem('audio_time_max')
     InputReg_S.value = localStorage.getItem('audio_time')
